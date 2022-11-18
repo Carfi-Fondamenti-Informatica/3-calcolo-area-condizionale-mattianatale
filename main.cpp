fbc7e91ec51965c7e272a2ca4e077abc699f2b0e
mattianatale <117930083+mattianatale@users.noreply.github.com>
2022-11-18 08:14:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
+int main(){
+        float a,b;
+        int o;
+        cin>> a >> b >> o;
+        switch (o) {
+            case 0:
+                cout<< (a*b)/2 << endl;
+                break;
+            case 1:
+                cout<< (a*a) << endl;
+                break;
+            case 2:
+                cout << (a*b)<< endl ;
+                break;
+            default:
+                cout << "opzione non valida";
+        }
+        return 0;
+    }
 
-int main() {
-   return 0;
-}
